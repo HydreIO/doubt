48d3cb7b0c8fceb2d90d6f5df65311b7c692e489
Sceat <sceat@hydre.io>
2020-05-06 22:36:07 +0200

    run format

--- src/cli.js
+++ src/cli.js
@@ -3,9 +3,7 @@ import glob from 'glob'
 import path from 'path'
 
 glob(
-    process.argv[2],
-    {},
-    (error, files) => {
+    process.argv[2], {}, (error, files) => {
       for (const file of files) import(path.resolve(file))
     },
 )
